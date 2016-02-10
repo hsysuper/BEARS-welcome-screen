441b7d98e5f1008acb5d205ceaf9655f53989010
Jack He <hsysuper@gmail.com>
2016-02-09 18:10:16 -0800

    Add main

---
+++ python/main.py
@@ -0,0 +1,22 @@
+"""
+
+Search for the nearest 10 BLE becons and return them as list of strings
+
+"""
+def discoverBLEBeacon():
+
+
+
+"""
+
+Returns user identity from a BLE MAC address
+
+"""
+def lookUpBeconIdentity(bleaddr):
+
+"""
+
+Look up user specific message to display
+
+"""
+def lookUpUserMessage():
